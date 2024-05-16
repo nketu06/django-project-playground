6b5f410ab1d2104dec1f0a6cf842c76ea780506b
Nishant ketu <nketu06@gmail.com>
2024-05-16 23:32:14 +0530

    pratice creating urls

--- playground/playground/urls.py
+++ playground/playground/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from course.views import course_name
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('course-name/', course_name,name='course_name'),
 ]
