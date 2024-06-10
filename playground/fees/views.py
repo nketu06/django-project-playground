c6f44a5e4a731ca49a9781f5195fa572c889dacb
Nishant ketu <nketu06@gmail.com>
2024-06-11 01:12:18 +0530

    add static file

--- playground/fees/views.py
+++ playground/fees/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def fees_detail(request):
-    return HttpResponse("<h1>your fee is free</h1>")
+    print(request,'------>',type(request))
+    return render(request,"fee_detail.html")
--- playground/playground/urls.py
+++ playground/playground/urls.py
@@ -15,10 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from course.views import course_name
-from django.urls import include
-import fees
 
 urlpatterns = [
     path('admin/', admin.site.urls),
