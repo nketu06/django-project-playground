189b255d4b6d9967d4ea05999e275e57542c426d
Nishant ketu <nketu06@gmail.com>
2024-06-13 00:34:54 +0530

    add tags

--- playground/fees/views.py
+++ playground/fees/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
 
 def fees_detail(request):
     print(request,'------>',type(request))
-    context={'name':'ketu','price':"zero"}
+    context={'name':'ketu','price':"zero","today":datetime.now()}
     return render(request,"fees/fee_detail.html",context)
