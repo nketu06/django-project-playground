70d92b41c81d08355285379f75315d4361f568ae
Nishant ketu <nketu06@gmail.com>
2024-07-07 22:35:02 +0530

    add student model

--- playground/result/models.py
+++ playground/result/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    id=models.IntegerField(primary_key=True)
+    name=models.CharField(max_length=30)
+    email=models.EmailField(max_length=30)
+    isPass=models.BooleanField()
+    comment= models.CharField(max_length=40,default="No Comment")
